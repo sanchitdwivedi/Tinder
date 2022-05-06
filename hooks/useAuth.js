cf3a99d69c53f89071dd14bff43e2088a55700ce
unknown <sanchitdwivedi123@gmail.com>
2022-05-06 10:40:18 +0530

    Before adding testing scripts

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -10,7 +10,8 @@ const config = {
     androidClientId: '40028622441-s4jadadee0qvn03upb98ns83rhsp5rfg.apps.googleusercontent.com',
     iosClientId: '40028622441-fa41964p0k5vilrhvqsgp50b748gtvb4.apps.googleusercontent.com',
     scopes: ["profile", "email"],
-    permissions: ["public_profile", "email", "gender", "location"]
+    permissions: ["public_profile", "email", "gender", "location"],
+    androidStandaloneAppClientId: '40028622441-b0lnfubqgbit5i8eo1sdb8u171dcb259.apps.googleusercontent.com'
 }
 
 export const AuthProvider = ({ children }) => {
